f59a79d92a6095e7960d3cd2bca17bbc58feb06f
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-01-06 08:33:27 +0100

    Create getters and setter to Seller model

--- src/main/java/com/ecommerce/demo/model/Seller.java
+++ src/main/java/com/ecommerce/demo/model/Seller.java
@@ -23,4 +23,59 @@ public class Seller {
   private String password;
   private List<Product> products = new ArrayList<Product>();
 
+  public Long getSellerId() {
+    return sellerId;
+  }
+
+  public void setSellerId(Long sellerId) {
+    this.sellerId = sellerId;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getMobileNo() {
+    return mobileNo;
+  }
+
+  public void setMobileNo(String mobileNo) {
+    this.mobileNo = mobileNo;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public List<Product> getProducts() {
+    return products;
+  }
+
+  public void setProducts(List<Product> products) {
+    this.products = products;
+  }
 }
