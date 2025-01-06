94751fd8f9d6c9d204ef60a764630cf4d0f3b0a8
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-01-06 08:33:18 +0100

    Create getters and setter to Product model

--- src/main/java/com/ecommerce/demo/model/Product.java
+++ src/main/java/com/ecommerce/demo/model/Product.java
@@ -19,4 +19,60 @@ public class Product {
   private String manufacture;
   private Integer quantity;
   private Seller seller;
+
+  public Long getProductId() {
+    return productId;
+  }
+
+  public void setProductId(Long productId) {
+    this.productId = productId;
+  }
+
+  public String getProductName() {
+    return productName;
+  }
+
+  public void setProductName(String productName) {
+    this.productName = productName;
+  }
+
+  public Double getPrice() {
+    return price;
+  }
+
+  public void setPrice(Double price) {
+    this.price = price;
+  }
+
+  public String getDescription() {
+    return Description;
+  }
+
+  public void setDescription(String description) {
+    Description = description;
+  }
+
+  public String getManufacture() {
+    return manufacture;
+  }
+
+  public void setManufacture(String manufacture) {
+    this.manufacture = manufacture;
+  }
+
+  public Integer getQuantity() {
+    return quantity;
+  }
+
+  public void setQuantity(Integer quantity) {
+    this.quantity = quantity;
+  }
+
+  public Seller getSeller() {
+    return seller;
+  }
+
+  public void setSeller(Seller seller) {
+    this.seller = seller;
+  }
 }
