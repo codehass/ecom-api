1b121b45504a18924e95b448ff88b1d58a045421
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-01-06 08:33:41 +0100

    Create getters and setter to UserSession model

--- src/main/java/com/ecommerce/demo/model/UserSession.java
+++ src/main/java/com/ecommerce/demo/model/UserSession.java
@@ -20,4 +20,51 @@ public class UserSession {
   private LocalDateTime sessionStartTime;
   private LocalDateTime sessionEndTime;
 
+  public Long getSessionId() {
+    return sessionId;
+  }
+
+  public void setSessionId(Long sessionId) {
+    this.sessionId = sessionId;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+
+  public Integer getUserId() {
+    return userId;
+  }
+
+  public void setUserId(Integer userId) {
+    this.userId = userId;
+  }
+
+  public String getUserType() {
+    return userType;
+  }
+
+  public void setUserType(String userType) {
+    this.userType = userType;
+  }
+
+  public LocalDateTime getSessionStartTime() {
+    return sessionStartTime;
+  }
+
+  public void setSessionStartTime(LocalDateTime sessionStartTime) {
+    this.sessionStartTime = sessionStartTime;
+  }
+
+  public LocalDateTime getSessionEndTime() {
+    return sessionEndTime;
+  }
+
+  public void setSessionEndTime(LocalDateTime sessionEndTime) {
+    this.sessionEndTime = sessionEndTime;
+  }
 }
